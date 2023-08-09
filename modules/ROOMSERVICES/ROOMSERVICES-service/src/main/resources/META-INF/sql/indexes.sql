57a5e3edd94db5ad216cc2707dd14f16701759fb
nayana.m05 <nayana.m05@TVMATP499038L.ad.infosys.com>
2023-08-09 18:05:03 +0530

    removing unwanted file

--- modules/ROOMSERVICES/ROOMSERVICES-service/src/main/resources/META-INF/sql/indexes.sql
+++
@@ -1,1 +0,0 @@
-create index IX_FC5652B on Infy_RoomType (RoomType[$COLUMN_LENGTH:75$]);
--- modules/custom-services/custom-services-service/src/main/resources/META-INF/sql/tables.sql
+++
@@ -1,4 +0,0 @@
-create table Hotel_Employee (
-	employeeId LONG not null primary key,
-	userName VARCHAR(75) null
-);
